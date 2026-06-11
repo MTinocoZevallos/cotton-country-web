4fa9d2014442043222be106e98ae455d986b4948
Manuel Tinoco <manuel.tinoco@cottoncountry.com.pe>
2026-06-11 17:18:46 -0500

    actualiza navegacion del header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,33 @@
-// components/Header.tsx
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="fixed top-0 w-full h-20 bg-[#01018B] flex items-center justify-between px-8 z-50 shadow-md">
-      {/* Logo a la izquierda */}
       <div className="flex items-center">
         <Link href="/">
-          <img src="/logo-cotton-blanco.png" alt="Cotton Country" className="h-10 w-auto" />
+          <img
+            src="/logo-cotton-blanco.png"
+            alt="Cotton Country"
+            className="h-10 w-auto"
+          />
         </Link>
       </div>
 
-      {/* Enlaces y botón a la derecha */}
       <nav className="flex items-center space-x-6">
-        <Link href="/blog" className="text-white font-medium hover:underline">
-          Blog
-        </Link>
-        <Link href="/uniformes" className="text-white font-medium hover:underline">
+        <Link
+          href="/uniformes"
+          className="text-white font-medium hover:underline"
+        >
           Uniformes
         </Link>
-        <Link href="/contacto" className="text-white font-medium hover:underline">
-          Contacto
+
+        <Link
+          href="/blog"
+          className="text-white font-medium hover:underline"
+        >
+          Blog
         </Link>
 
-        {/* Botón de email solo con icono */}
         <a
           href="mailto:info@cottoncountry.com.pe"
           className="bg-white text-[#01018B] p-2 rounded flex items-center justify-center hover:bg-gray-200"
