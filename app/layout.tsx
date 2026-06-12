27beda4379c9f6afedfaaa6a397e4c7db690a21b
Manuel Tinoco <manuel.tinoco@cottoncountry.com.pe>
2026-06-12 12:40:20 -0500

    configura metadata base y open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,25 @@ import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://cottoncountry.com.pe"),
+
   title: "Cotton Country | Uniformes Corporativos",
+
   description:
     "Uniformes corporativos, institucionales e industriales diseñados y producidos en Perú.",
+
   verification: {
     google: "zpHF0Zr6rY5sswtDJjqndC4cUnDXGsfkldktUVcTv2g",
+  },
+
+  openGraph: {
+    title: "Cotton Country | Uniformes Corporativos",
+    description:
+      "Uniformes corporativos, institucionales e industriales diseñados y producidos en Perú.",
+    url: "https://cottoncountry.com.pe",
+    siteName: "Cotton Country",
+    locale: "es_PE",
+    type: "website",
   },
 }
 
