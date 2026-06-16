834ce85a0e57a06395edb2636113b0bae8ddafb1
Manuel Tinoco <manuel.tinoco@cottoncountry.com.pe>
2026-06-16 14:10:00 -0500

    instala google analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import Script from "next/script"
 import "./globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
@@ -34,6 +35,20 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className="flex flex-col min-h-screen">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-7VEMKX1RHX"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7VEMKX1RHX');
+          `}
+        </Script>
+
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
